d4efcd4654fb6b2836d9ab11f3dccde01e47b8f5
Colin Wright <cjwright@email.wm.edu>
2019-10-18 21:19:04 -0400

    Testing first commit. Added beginings of a class to handle 2d animation

---
+++ application/include/Animation.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class animation
+{
+public:
+  animation();
+  void Update();
+};
---
+++ application/src/Animation.cpp
@@ -0,0 +1,12 @@
+#include "animation.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+animation::animation()
+{
+
+}
+void animation::Update()
+{
+  
+}
