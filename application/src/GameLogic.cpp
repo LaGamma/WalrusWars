95b220915ba7d2db9011b6d2ed2f3491679eafa6
Ayush Joshi <ajoshi@email.wm.edu>
2019-10-23 15:29:15 -0400

    started doing some stuff, wip

--- application/src/GameLogic.cpp
+++ application/src/GameLogic.cpp
@@ -33,9 +33,9 @@ void GameLogic::update(float dSec) {
         //if (w1_pos.x > 800.0f || w1_pos.y > 600.0f || w1_pos.x < 0 || w1_pos.y < 0)
         //    handlePlayerDeath(1);
         //if (w2_pos.x > 800.0f || w2_pos.y > 600.0f || w2_pos.x < 0 || w2_pos.y < 0)
-        if (stage.getTile((w1_pos.x)/20, (w1_pos.y)/20) == 0)
+        if (stage.getTile((w1_pos.x)/20, (w1_pos.y)/20, progression) == 0)
             handlePlayerDeath(1);
-        if (stage.getTile((w2_pos.x)/20, (w2_pos.y)/20) == 0)
+        if (stage.getTile((w2_pos.x)/20, (w2_pos.y)/20, progression) == 0)
             handlePlayerDeath(2);
 
         sf::Vector2f posDiff = w1_pos - w2_pos;
@@ -125,8 +125,8 @@ void GameLogic::handlePlayerDeath(int x) {
         progression = 0;
 	}
 	// respawn
-    walrus1.spawn(sf::Vector2f(400.0f, 200.0f));
-    walrus2.spawn(sf::Vector2f(400.0f, 400.0f));
+    walrus1.spawn(sf::Vector2f(400.0f, 250.0f));
+    walrus2.spawn(sf::Vector2f(400.0f, 350.0f));
 }
 
 void GameLogic::togglePause() {
--- application/src/Stage.cpp
+++ application/src/Stage.cpp
@@ -12,24 +12,69 @@ void Stage::generateMap(){
 	for(i = 0; i<40; i++){
 		for(j = 0; j<30; j++){
 			if((i>10 && i<=30)&&(j>5 && j<=25)){
-				stageTiles[i][j] = 1;
-			} else {
+				centerStage[i][j] = 1;
+			}
+			else if((i>14 && i<=26)&&(j>9&&j<=21)){
+				leftStage1[i][j] = 1;
+				rightStage1[i][j] = 1;
+			}
+			else if((i>16 && i<=24)&&(j>11&&j<=19)){
+				leftStage2[i][j] = 1;
+				rightStage2[i][j] = 1;
+			}
+			else if((j>13&&j<=17)){
+				centerStage[i][j] = 1;
+				leftStage1[i][j] = 1;
+				rightStage1[i][j] = 1;
+			}
+			else if((j>13&&j<=17)&&(i>20)){
+				leftStage2[i][j] = 1;
+			}
+			else if((j>13&&j<=17)&&(i<20)){
+				rightStage2[i][j] = 1;
+			}
+			else {
 			    long r = rand() % 10 + 1;
 			    if (r > 6) {
-			        stageTiles[i][j] = 1;
+			        centerStage[i][j] = 1;
+							leftStage1[i][j] = 1;
+							rightStage1[i][j] = 1;
+							leftStage2[i][j] = 1;
+							rightStage2[i][j] = 1;
 			    } else {
-			        stageTiles[i][j] = 0;
+			        centerStage[i][j] = 0;
+							leftStage1[i][j] = 0;
+							rightStage1[i][j] = 0;
+							leftStage2[i][j] = 0;
+							rightStage2[i][j] = 0;
 			    }
 			    if (i == 31 || j == 26 || i == 10 || j == 5) {
-			        stageTiles[i][j] = 0;
+			        centerStage[i][j] = 0;
 			    }
-			} //this is a hardcoded loop that should create an island of ice
+
+			}
+		std::cout<<centerStage[i][j]; //this is a hardcoded loop that should create an island of ice
 		}
 	}
 
 }
-int Stage::getTile(int x, int y){
-	return stageTiles[x][y];
+int Stage::getTile(int x, int y, int stage){
+	if(stage == 0){
+		return centerStage[x][y];
+	}
+	else if(stage == -1){
+		return leftStage1[x][y];
+	}
+	if(stage == -2){
+		return leftStage2[x][y];
+	}
+	if(stage == 1){
+		return rightStage1[x][y];
+	}
+	if(stage == 2){
+		return rightStage2[x][y];
+	}
+
 }
 
 
