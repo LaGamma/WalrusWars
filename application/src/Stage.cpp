c9ccf6b9c7a76252c1fc7cac97df6818b5919e50
Ayush Joshi <ajoshi@email.wm.edu>
2019-10-25 16:40:52 -0400

    new changes to map creation algorithm
    now, instead of stages that are explicitly different sizes, each stage is actually the same size, but each stage (during generation) has a random chance to put some holes in the ice along the edges. As you get further from the center, the area where holes can be generated gets bigger. So this gives the illusion of the stages getting smaller in a more subtle way without the super abrupt changes in size

--- application/src/Stage.cpp
+++ application/src/Stage.cpp
@@ -9,22 +9,39 @@ Stage::Stage(){
 void Stage::generateMap(){
 	int i;
 	int j;
+	long r;
 	for(i = 0; i<40; i++){
 		for(j = 0; j<30; j++){
 			if((i>8 && i<=32)&&(j>2 && j<=28)){
 				centerStage[i][j] = 1;
-				if((i>12 && i<=28)&&(j>6&&j<=24)){
-					leftStage1[i][j] = 1;
-					rightStage1[i][j] = 1;
-					if((i>14 && i<=26)&&(j>9&&j<=21)){
-						leftStage2[i][j] = 1;
-						rightStage2[i][j] = 1;
-					}
+				leftStage1[i][j] = 1;
+				leftStage2[i][j] = 1;
+				rightStage1[i][j] = 1;
+				rightStage2[i][j] = 1;
+				r = rand() % 20;
+				if((r>15)&&(((j>2&&j<4)||(j>26&&j<=28))||((i>8&&i<10)||(i>30&&i<=32)))){
+					centerStage[i][j] = 0;
 				}
-			}
+				if((r>12)&&(((j>2&&j<6)||(j>24&&j<=28))||((i>8&&i<12)||(i>28&&i<=32)))){
+					leftStage1[i][j] = 0;
+					rightStage1[i][j] = 0;
+				}
+				if((r>9)&&(((j>2&&j<8)||(j>22&&j<=28))||((i>8&&i<14)||(i>26&&i<=32)))){
+					leftStage2[i][j] = 0;
+					rightStage2[i][j] = 0;
+				}
+				//if((i>10 && i<=30)&&(j>6&&j<=24)){
+				//	leftStage1[i][j] = 1;
+				//	rightStage1[i][j] = 1;
+				//	if((i>12 && i<=28)&&(j>9&&j<=21)){
+				//		leftStage2[i][j] = 1;
+				//		rightStage2[i][j] = 1;
+				//	} //creates the smallest stage
+				//} // creates the second-to-last stages
+			} // creates the center stage
 			else {
-			    long r = rand() % 10 + 1;
-			    if (r > 6) {
+			    r = rand() % 10 + 1;
+			    if (r > 8) {
 			        centerStage[i][j] = 1;
 							leftStage1[i][j] = 1;
 							rightStage1[i][j] = 1;
@@ -39,8 +56,7 @@ void Stage::generateMap(){
 			    }
 			    if (i == 31 || j == 26 || i == 10 || j == 5) {
 			        centerStage[i][j] = 0;
-			    }
-
+			    } //adds some random ice bits that 'float' in the sea
 
 			}
 			if(j>13&&j<=17){
@@ -52,7 +68,7 @@ void Stage::generateMap(){
 				}
 				if(i<20){
 					rightStage2[i][j] = 1;
-				}
+				} //builds a bridge on each stage
 			} //this is a hardcoded loop that should create an island of ice
 		}
 	}
