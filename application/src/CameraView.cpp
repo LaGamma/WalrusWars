6d6c3f8292cd2ab609974102ab4a857c19273e58
LaGamma <nplagamma@email.wm.edu>
2019-10-26 01:13:43 -0400

    reset to 2 player controls for now

--- application/src/CameraView.cpp
+++ application/src/CameraView.cpp
@@ -274,7 +274,7 @@ void CameraView::processInput(sf::RenderWindow &window, GameLogic &logic, float 
                             main_menu_selection = 'S';
                     } else if (Event.key.code == sf::Keyboard::Return) {
                         std::cout << "start game!" << std::endl;
-                        createControllers(1);
+                        createControllers(2);
                         logic.playGame();
                     } else if (Event.key.code == sf::Keyboard::P && logic.getState() == GameLogic::GameState::pauseMenu) {
                         std::cout << "toggle pause" << std::endl;
