b99a682973d43b9ad4b6a054c2d127678e37e8b7
Ayush Joshi <ajoshi@email.wm.edu>
2019-11-22 14:58:06 -0500

    okay now I think the crashes are fixed
    TO DO: make the motion to the next stage when the bot wins a lot smoother, make the movement towards "safe" positions also look smoother

--- application/src/BotController.cpp
+++ application/src/BotController.cpp
@@ -14,7 +14,7 @@ BotController::BotController() {
 void BotController::update(sf::RenderWindow &window, GameLogic &logic, float dSec, int playerNum, Animation &anim) {
 
 
-    float bot_handicap = 1;  // higher number == slower bot
+    float bot_handicap = .5;  // higher number == slower bot
     sf::Vector2f w1_pos = logic.walrus1.getPos();
     sf::Vector2f w2_pos = logic.walrus2.getPos();
     sf::Vector2f w1_vel = logic.walrus1.getVel();
@@ -167,12 +167,12 @@ void BotController::calculatePath(GameLogic &logic, int playerNum){
 
     std::cout<<state<<"\n";
     if(playerNum==1){
-      x = int(logic.walrus2.getPos().x/20);
-      y = int(logic.walrus2.getPos().y/20);
+      x = int(logic.walrus2.getPos().x/ICE_BLOCKS_SIZE_X);
+      y = int(logic.walrus2.getPos().y/ICE_BLOCKS_SIZE_Y);
     }
     else {
-      x = int(logic.walrus1.getPos().x/20);
-      y = int(logic.walrus1.getPos().y/20);
+      x = int(logic.walrus1.getPos().x/ICE_BLOCKS_SIZE_X);
+      y = int(logic.walrus1.getPos().y/ICE_BLOCKS_SIZE_Y);
     }
   }
   else{
@@ -303,6 +303,10 @@ void BotController::calculatePath(GameLogic &logic, int playerNum){
   j = w_pos.y/ICE_BLOCKS_SIZE_Y;
   if(!logic.walrus2.isDead()){
     while((i!=x)||(j!=y)){
+      //std::cout<<"i = "<<i<<"\n";
+      //std::cout<<"j = "<<j<<"\n";
+      //std::cout<<"x = "<<x<<"\n";
+      //std::cout<<"y = "<<y<<"\n";
       if((cellDetails[x][y].pi<x)&&(cellDetails[x][y].pj<y)) {  // top left corner (1)
         directionStack.push(1);
       }
@@ -327,9 +331,11 @@ void BotController::calculatePath(GameLogic &logic, int playerNum){
       else if((cellDetails[x][y].pi>x)&&(cellDetails[x][y].pj>y)) {  // bottom center (8)
         directionStack.push(8);
       }
-      std::cout<<"looping\n";
       x = cellDetails[x][y].pi;
       y = cellDetails[x][y].pj;
+      if(x==-1||y==-1){
+        break;
+      }
     }
   }
 }
